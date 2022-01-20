5d1397895b943289a0a5aca75c52ee8621664904
변재호 <bjho606@naver.com>
2022-01-20 18:20:07 +0900

    gitignore finish

--- config/secret.js
+++ config/secret.js
@@ -3,4 +3,12 @@
 module.exports = {
     'jwtsecret' : '',
 
+    'CLIENT_ID' :  'd78a94142d0c7f4e304d6a19a3b20844',
+    'REDIRECT_URI' : 'http://localhost:3000/kakao/oauth',
+    'KAKAO_AUTH_URL' : 'https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code'
 };
+
+// const CLIENT_ID = 'd78a94142d0c7f4e304d6a19a3b20844';
+// const REDIRECT_URI = 'http://localhost:3000/kakao/oauth';
+//
+// export const KAKAO_AUTH_URL = 'https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code'
