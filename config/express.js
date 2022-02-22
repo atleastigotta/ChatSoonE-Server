12ab4bb368571ddbd8a6ef318525303de52ec223
변재호 <bjho606@naver.com>
2022-02-22 23:48:06 +0900

    PayloadTooLargeError: request entity too large 에러 고치기 test

--- config/express.js
+++ config/express.js
@@ -7,9 +7,14 @@ module.exports = function () {
 
     app.use(compression());
 
-    app.use(express.json());
+    app.use(express.json({
+        limit: '50mb'
+    }));
 
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({
+        limit: '50mb',
+        extended: true
+    }));
 
     app.use(methodOverride());
 
